3342e3705af503c77b121e98e708b0000291b8d8
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-05-02 22:12:10 +0200

    #4 Page d'accueil + Barre de navigation

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php // Ce fichier est la page d'accueil ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Page d'accueil</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!-- Barre de navigation -->
+<ul>
+    <li><a class="active" href="">Accueil</a></li>
+    <li><a href="">Connexion</a></li>
+    <li><a href="">Inscription</a></li>
+</ul>
+<!-- Le reste du contenu -->
+<section class="section-accueil">
+<p>Hi !</p>
+</section>
+</body>
+</html>
+
+
